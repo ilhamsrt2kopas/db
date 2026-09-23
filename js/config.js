c332f6df8e10c86043f9cb2e60dc5aba064f8e71
ilhamsrt2kopas <ilham.srt2@gmail.com>
2026-09-23 14:14:28 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 /**
  * config.js - Ganti API_URL dengan Web App URL dari Google Apps Script
  */
-export const API_URL = 'https://script.google.com/macros/s/GANTI_DENGAN_DEPLOYMENT_ID/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycby7ZrSlUDQT09EZmxXVf2Esdr2ViJ0d9O-AL1xlO3MI2s6RBCqbzpa4qY-kbqwJn1kT/exec';
 
 export const APP_NAME = 'Sekolah Rakyat Kota Pasuruan';
 export const TAGLINE = 'Cerdas Bersama, Tumbuh Setara';
